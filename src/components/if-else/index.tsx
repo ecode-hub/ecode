316b739c0e65b85874e16e33db10789f9c81077c
wangyulue <1206180097@qq.com>
2020-05-01 13:48:01 +0800

    fix(*):nginx open gzip

---
+++ src/components/if-else/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+interface IProps {
+  if: boolean;
+  else?: JSX.Element;
+}
+
+function IfElse(props: IProps & React.PropsWithChildren<IProps>) {
+  const { children } = props;
+  const ifCondition = props.if;
+  const elseElement = props.else;
+
+  return ifCondition ? <>{children}</> : (elseElement || null);
+}
+
+export {
+  IfElse
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,13 @@ import { Header } from './header';
 import { Wraper as HeaderWraper } from './header/wraper';
 import { ErrorBoundary } from './error-boundary';
 import { SafeContainer } from './safe-container';
+import { IfElse } from './if-else';
 
 export {
   Markdown,
   Header,
   HeaderWraper,
   ErrorBoundary,
-  SafeContainer
+  SafeContainer,
+  IfElse
 };
